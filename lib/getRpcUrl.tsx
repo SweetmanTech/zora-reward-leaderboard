bf889a9a4b21c6589d75b236a4404bd811638495
Sweets Sweetman <sweetmantech@gmail.com>
2023-09-30 17:59:40 -0300

    I simplify getRpcUrl lib.

--- lib/getRpcUrl.tsx
+++ lib/getRpcUrl.tsx
@@ -2,13 +2,10 @@ import { zora } from "@wagmi/core/chains"
 import getAlchemyBaseUrl from "./alchemy/getAlchemyBaseUrl"
 
 const getRpcUrl = (chainId) => {
-  let endpoint
   if (chainId === zora.id) {
-    endpoint = "https://rpc.zora.energy"
-  } else {
-    endpoint = `${getAlchemyBaseUrl(chainId)}v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+    return "https://rpc.zora.energy"
   }
-  return endpoint
+  return `${getAlchemyBaseUrl(chainId)}v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
 }
 
 export default getRpcUrl
