2e00ba7d1f5cda815a66f0110f0eaca52894aefb
Sweets Sweetman <sweetmantech@gmail.com>
2023-09-29 23:33:45 -0300

    I fix build errors.

--- components/LeaderboardPage/LeaderboardPage.tsx
+++ components/LeaderboardPage/LeaderboardPage.tsx
@@ -1,9 +1,9 @@
+import { formatEther } from "ethers/lib/utils"
 import Layout from "../Layout"
 import TimeFilter from "./TimeFilter"
 import LeaderboardTable from "./LeaderboardTable"
 import useLeaderboard from "../../hooks/useLeaderboard"
 import LoadingModal from "../LoadingModal"
-import { formatEther, parseEther } from "ethers/lib/utils"
 
 const LeaderboardPage = () => {
   const { collectors, numberOfDays, setNumberOfDays, loading, zoraFees, creatorFees } =
