39abaab77c98bea03e836669adac07292f265e37
sameer rasa <crazzywizard@gmail.com>
2024-01-16 22:14:55 -0500

    trying a specific named endpoint

--- lib/getProtocolRewardsLeaderboard.tsx
+++ lib/getProtocolRewardsLeaderboard.tsx
@@ -9,9 +9,12 @@ const getProtocolRewardsLeaderboard = async (numberOfDays) => {
     { id: zora.id, key: "zoraReward" },
   ]
 
-  const response = await axios.get("/api/get/indexedData", {
-    params: { days: numberOfDays },
-  })
+  const response = await axios.get(
+    "https://zorarewards-git-sameer-useindexer-sweetmantech.vercel.app//api/get/indexedData",
+    {
+      params: { days: numberOfDays },
+    },
+  )
   const allData = response.data.map((item) => ({
     ...item,
     rewardType: `${item.chain}Reward`,
